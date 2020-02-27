8069c747b63026a490d9f24193bef708c8516f37
jrgant <jason.gantenberg@gmail.com>
2020-02-26 21:45:36 -0500

    quick diagnostics

--- 08-NetDx.R
+++ 08-NetDx.R
@@ -1,8 +1,17 @@
+# %% SETUP ---------------------------------------------------------------------
+
 suppressMessages(library(EpiModel))
 
-
 mdx <- readRDS("netest/dx_main.Rds")
-cpx <- readRDS("netest/dx_casl.Rds")
+cdx <- readRDS("netest/dx_casl.Rds")
+
+
+# %% PLOT SIMULATED NETWORKS ---------------------------------------------------
+
+# Quick Diagnostics
 
 plot(mdx, qnts = 0.95)
-plot(cpx, qnts = 0.95)
+plot(cdx, qnts = 0.95)
+
+print(mdx)
+print(cdx)
