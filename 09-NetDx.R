7d7614c82dba9760b50d118750f40a0b449d6a1f
jrgant <jason.gantenberg@gmail.com>
2020-06-29 22:21:04 +0100

    update dx file path

--- 09-NetDx.R
+++ 09-NetDx.R
@@ -2,16 +2,12 @@
 
 suppressMessages(library(EpiModel))
 
-mdx <- readRDS("netest/dx_main.Rds")
-cdx <- readRDS("netest/dx_casl.Rds")
-idx <- readRDS("netest/dx_inst.Rds")
-
-
+dx <- readRDS("netest/netdx.Rds")
 
 # %% PLOT SIMULATED NETWORKS ---------------------------------------------------
 
 # Quick Diagnostics
-plot(mdx, qnts = 0.95, sim.col = "cornflowerblue")
+plot(dx[[1]], qnts = 0.95, sim.col = "cornflowerblue")
 plot(cdx, qnts = 0.95, sim.col = "cornflowerblue")
 plot(idx, qnts = 0.95, sim.col = "cornflowerblue")
 
