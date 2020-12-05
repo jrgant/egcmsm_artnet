5dac47e30bbbe4c972b8cf504f479781370a2c17
jrgant <jason.gantenberg@gmail.com>
2020-12-05 18:51:03 -0500

    reorganize network estimation and diagnostics

--- 08-NetEst.R
+++ 08-NetEst.R
@@ -17,7 +17,6 @@ netstats <- readRDS(here::here("netstats", "netstats.Rds"))
 ################################################################################
 
 mcmc.maxiterations <- 500
-use_ncores <- 6
 
 
 ################################################################################
@@ -49,19 +48,6 @@ nw <- set.vertex.attribute(nw, "role.class", netstats$attr$role.class)
 ################################################################################
                             ## MAIN PARTNERSHIPS ##
 ################################################################################
-
-main_formation_full <-
-  ~ edges +
-    nodefactor("age.grp", levels = NULL) +
-    nodefactor("race", levels = NULL) +
-    nodefactor("deg.casl", levels = NULL) +
-    nodefactor("diag.status", levels = NULL) +
-    degrange(from = 3) +
-    concurrent +
-    nodematch("race") +
-    nodematch("age.grp") +
-    nodematch("diag.status") +
-    nodematch("role.class", levels = c(1, 2))
 
 main_formation <-
   ~ edges +
@@ -111,43 +97,10 @@ netest_main <- netest(
   set.control.ergm = control.ergm(MCMLE.maxit = mcmc.maxiterations)
 )
 
-summary(netest_main)
-
-mcmc.diagnostics(netest_main$fit)
-
-dx_main <- netdx(
-  netest_main,
-  nsims = 10,
-  nsteps = 100,
-  ncores = use_ncores,
-  skip.dissolution = FALSE,
-  nwstats.formula = main_formation_full
-)
-
-## NOTE: Need to burn in for a long time to assess duration match
-## will be biased downward for early time steps.
-## plot(dx_main, qnts = 0.95)
-## plot(dx_main, type = "duration")
-
-print(dx_main)
-
 
 ################################################################################
                            ## CASUAL PARTNERSHIPS ##
 ################################################################################
-
-casl_formation_full <-
-  ~ edges +
-    nodefactor("age.grp", levels = NULL) +
-    nodefactor("race", levels = NULL) +
-    nodefactor("deg.main", levels = NULL) +
-    nodefactor("diag.status", levels = NULL) +
-    degrange(from = 6) +
-    concurrent +
-    nodematch("race", levels = NULL) +
-    nodematch("age.grp") +
-    nodematch("diag.status") +
-    nodematch("role.class", levels = c(1, 2))
 
 casl_formation <-
   ~ edges +
@@ -197,38 +150,10 @@ netest_casl <- netest(
   set.control.ergm = control.ergm(MCMLE.maxit = mcmc.maxiterations)
 )
 
-summary(netest_casl)
-
-mcmc.diagnostics(netest_casl$fit)
-
-dx_casl <- netdx(
-  netest_casl,
-  nsims = 10,
-  nsteps = 100,
-  ncores = use_ncores,
-  skip.dissolution = FALSE,
-  nwstats.formula = casl_formation_full
-)
-
-## NOTE: Need to burn in for a long time to assess duration match
-## will be biased downward for early time steps.
-## plot(dx_casl, qnts = 0.95)
-## plot(dx_casl, "duration")
-## print(dx_casl)
-
 
 ################################################################################
                           ## ONE-TIME PARTNERSHIPS ##
 ################################################################################
-
-inst_formation_full <-
-  ~ edges +
-    nodefactor("race", levels = NULL) +
-    nodefactor("age.grp", levels = NULL) +
-    nodefactor("deg.main", levels = NULL) +
-    nodefactor("deg.casl", levels = NULL) +
-    nodefactor("diag.status", levels = NULL) +
-    nodematch("role.class", levels = c(1, 2))
 
 inst_formation <-
   ~ edges +
@@ -267,21 +192,6 @@ netest_inst <- netest(
   set.control.ergm = control.ergm(MCMLE.maxit = mcmc.maxiterations)
 )
 
-summary(netest_inst)
-mcmc.diagnostics(netest_inst$fit)
-
-dx_inst <- netdx(
-  netest_inst,
-  nsims = 1000,
-  ncores = use_ncores,
-  skip.dissolution = FALSE,
-  nwstats.formula = inst_formation_full,
-  dynamic = FALSE
-)
-
-## plot(dx_inst, sim.lines = TRUE, alpha = 0.4)
-## print(dx_inst)
-
 
 # %% Write ------------------------------------------------------------------
 
@@ -291,7 +201,4 @@ netest_out <- list(
   fit_inst = netest_inst
 )
 
-dx_out <- list(dx_main, dx_casl, dx_inst)
-
 saveRDS(netest_out, "netest/netest.Rds")
-saveRDS(dx_out, "netest/netdx.Rds")
--- 09-NetDx.R
+++ 09-NetDx.R
@@ -1,8 +1,87 @@
 # %% SETUP ---------------------------------------------------------------------
 
 suppressMessages(library(EpiModel))
+est <- readRDS("netest/netest.Rds")
 
-dx <- readRDS("netest/netdx.Rds")
+use_ncores <- 16
+n_networks <- 10
+n_timesteps <- 52 * 10
+
+
+# %% MAIN PARTNERSHIPS ---------------------------------------------------------
+
+main_formation_full <-
+  ~ edges +
+    nodefactor("age.grp", levels = NULL) +
+    nodefactor("race", levels = NULL) +
+    nodefactor("deg.casl", levels = NULL) +
+    nodefactor("diag.status", levels = NULL) +
+    degrange(from = 3) +
+    concurrent +
+    nodematch("race") +
+    nodematch("age.grp") +
+    nodematch("diag.status") +
+    nodematch("role.class", levels = c(1, 2))
+
+dx_main <- netdx(
+  est[[1]],
+  nsims = n_networks,
+  nsteps = n_timesteps,
+  ncores = use_ncores,
+  skip.dissolution = FALSE,
+  nwstats.formula = main_formation_full
+)
+
+saveRDS(dx_main, "netest/netdx_main.Rds")
+
+# %% CASUAL PARTNERSHIPS -------------------------------------------------------
+
+casl_formation_full <-
+  ~ edges +
+    nodefactor("age.grp", levels = NULL) +
+    nodefactor("race", levels = NULL) +
+    nodefactor("deg.main", levels = NULL) +
+    nodefactor("diag.status", levels = NULL) +
+    degrange(from = 6) +
+    concurrent +
+    nodematch("race", levels = NULL) +
+    nodematch("age.grp") +
+    nodematch("diag.status") +
+    nodematch("role.class", levels = c(1, 2))
+
+dx_casl <- netdx(
+  est[[2]],
+  nsims = n_networks,
+  nsteps = n_timesteps,
+  ncores = use_ncores,
+  skip.dissolution = FALSE,
+  nwstats.formula = casl_formation_full
+)
+
+saveRDS(dx_casl, "netest/netdx_casl.Rds")
+
+# %% ONE-TIME CONTACTS ---------------------------------------------------------
+
+inst_formation_full <-
+  ~ edges +
+    nodefactor("race", levels = NULL) +
+    nodefactor("age.grp", levels = NULL) +
+    nodefactor("deg.main", levels = NULL) +
+    nodefactor("deg.casl", levels = NULL) +
+    nodefactor("diag.status", levels = NULL) +
+    nodematch("role.class", levels = c(1, 2))
+
+dx_inst <- netdx(
+  est[[3]],
+  nsims = 10000,
+  nsteps = n_timesteps,
+  ncores = use_ncores,
+  skip.dissolution = FALSE,
+  nwstats.formula = inst_formation_full,
+  dynamic = FALSE
+)
+
+saveRDS(dx_inst, "netest/netdx_inst.Rds")
 
 # %% PLOT SIMULATED NETWORKS ---------------------------------------------------
 
