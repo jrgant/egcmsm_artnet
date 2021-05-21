076ebef330533a0d20f342b682c2a3d02fe39bd3
jrgant <jason.gantenberg@gmail.com>
2021-05-21 13:05:10 -0400

    put network viz in its own script

--- 09-NetDx.R
+++ 09-NetDx.R
@@ -96,24 +96,3 @@ dx_inst <- netdx(
 saveRDS(dx_inst, "netest/netdx_inst.Rds")
 dx_inst <- NULL
 gc()
-
-
-# %% PLOT SIMULATED NETWORKS ---------------------------------------------------
-
-suppressMessages(library(EpiModelHIV))
-
-dx <- list(
-  readRDS("netest/netdx_main.Rds"),
-  readRDS("netest/netdx_casl.Rds"),
-  readRDS("netest/netdx_inst.Rds")
-)
-
-plotdx <- function(network, color = "skyblue") {
-  nt <- c("main", "casual", "inst")
-  plot(dx[[grep(network, nt)]], qnts = 0.95, sim.col = color)
-}
-
-plotdx("main")
-plotdx("casual")
-plotdx("inst")
-plot(dx[[3]], sim.lines = TRUE)
